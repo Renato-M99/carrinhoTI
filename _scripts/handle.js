0928344ec69152e6a55db62f48a57949f38f5793
Renato <renatojunior7@hotmail.com>
2024-06-21 22:23:13 -0300

    btn clear - inicial

--- _scripts/handle.js
+++ _scripts/handle.js
@@ -1,5 +1,6 @@
 const addBtn = document.querySelector(".addBtn");
 const clear = document.querySelector(".clearBtn");
+
 let options = Array.from(document.querySelectorAll("option"));
 let products = document.querySelector(".products");
 let addedProducts = Array.from(document.querySelectorAll(".showProduct"));
@@ -8,23 +9,24 @@ let productCount = 0;
 /* Checando se armazenou os itens no array */
 console.log(products);
 addProduct();
+clearBtn();
 function addProduct() {
     addBtn.addEventListener("click", (e) => {
-
         let qtd = document.querySelector(".qtd").value;
+
         let item = document.createElement('li');
-       
+
 
         productCount++;
 
-       
 
-        
+
+
         if (qtd != '') {
-            
+
             document.querySelector(".showProduct").append(item);
             item.textContent = `${qtd}x ${options[products.value].textContent}`;
-            
+
             addedProducts.push(item);
 
 
@@ -35,6 +37,15 @@ function addProduct() {
     })
 }
 
-
+function clearBtn() {
+    clear.addEventListener("click", () => {
+        addedProducts.pop();
+        
+        document.querySelector(".qtd").value = '';
+        document.querySelector("ul").removeChild();
+  
+    })
+  
+}
 
 
